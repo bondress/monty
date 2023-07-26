cb56f54f542fb510f68f8eeff4f9aef05bcd4788
Laide Endeley <endeleylaide@gmail.com>
2023-07-26 04:11:24 -0700

    Fixed Betty error in Task 0

--- push_pall.c
+++ push_pall.c
@@ -11,28 +11,28 @@
  */
 void push(stack_t **stack, unsigned int line_cnt)
 {
-        char *n = global.arg;
+	char *n = global.arg;
 
-        if ((is_digit(n)) == 0)
-        {
-                fprintf(stderr, "L%d: usage: push integer\n", line_cnt);
-                exit(EXIT_FAILURE);
-        }
+	if ((is_digit(n)) == 0)
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", line_cnt);
+		exit(EXIT_FAILURE);
+	}
 
-        if (global.d_struct == 1)
-        {
-                if (!add_no(stack, atoi(global.arg)))
-                {
-                        exit(EXIT_FAILURE);
-                }
-        }
-        else
-        {
-                if (!queue_no(stack, atoi(global.arg)))
-                {
-                        exit(EXIT_FAILURE);
-                }
-        }
+	if (global.d_struct == 1)
+	{
+		if (!add_no(stack, atoi(global.arg)))
+		{
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		if (!queue_no(stack, atoi(global.arg)))
+		{
+			exit(EXIT_FAILURE);
+		}
+	}
 }
 
 /**
@@ -44,5 +44,5 @@ void push(stack_t **stack, unsigned int line_cnt)
  */
 void pall(stack_t **stack, unsigned int line_cnt __attribute__((unused)))
 {
-        print_st(*stack);
+	print_st(*stack);
 }
