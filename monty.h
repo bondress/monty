d430e96bd0d3dfde440a0dd5d697ff07a28ecb97
Laide Endeley <endeleylaide@gmail.com>
2023-07-26 10:16:07 -0700

    Task 11

--- monty.h
+++ monty.h
@@ -18,6 +18,7 @@
 			{"div", _div},	\
 			{"mul", _mul},	\
 			{"mod", mod},	\
+			{"pchar", pchar},	\
 		{                     \
 			NULL, NULL      \
 		}                     \
@@ -84,6 +85,7 @@ void _sub(stack_t **stack, unsigned int line_cnt);
 void _div(stack_t **stack, unsigned int line_cnt);
 void _mul(stack_t **stack, unsigned int line_cnt);
 void mod(stack_t **stack, unsigned int line_cnt);
+void pchar(stack_t **stack, unsigned int line_cnt);
 
 void opcode(stack_t **stack, char *str, unsigned int line_cnt);
 
